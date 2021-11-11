ea341f6887c27fb12be23b60f18da39a8687bd0b
Dorcas Olajide <50446530+DorcasOJ@users.noreply.github.com>
2021-11-11 01:19:31 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,35 +2,18 @@ from flask import Flask, redirect, render_template, request, Markup, jsonify
 from os import path, walk
 import random, time
 import requests
-from getData import quote_list, get_news, CovidData
+from .getData import quote_list, CovidData
 import pandas as pd, numpy as np
 
 app = Flask(__name__)
-
-api_key = 'f69b33c1639044108e76fc692e1bd9d6'
 
 
 @app.route('/')
 def home_page():
     # get quotes
-    quotes = quote_list()
+    quotes = quote_list()    
 
-    #get news from api
-    news, chan, pun = get_news(api_key)
-    news_dict = {'news':news, 'chan':chan, 'pun': pun}
-    
-    # get covid overall data
-    covid_data = CovidData()
-    covid_overall = covid_data.get_covid_overall_data()
-    if covid_overall is None:
-        covid_overall = pd.read_csv('data/covid_overall.csv')
-    covid_overall_dict = {'labels': covid_overall.Info.values, 'values': covid_overall.Data.values}
-    colour_dict = {'Confirmed Cases':'rgb(16, 145, 231)','Active Cases':'goldenrod', 'Discharged Cases':'rgb(43, 136, 43)', 'Deaths':'rgb(233, 16, 16)'}
-        
-
-    return render_template('home.html', health_quote = random.choice(quotes), 
-    news_dict = news_dict, covid_overall_dict = covid_overall_dict, 
-    colour_dict = colour_dict)
+    return render_template('home.html', health_quote = random.choice(quotes))
 
 @app.route('/contact')
 def contact_page():
